607f2c40ca390e5fe34c34ac46f8fb40bf8ccb6e
peter <binmonk@gmail.com>
2016-02-17 15:38:22 +0300

    add packages

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', FALSE),
 
     /*
     |--------------------------------------------------------------------------
@@ -155,6 +155,13 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laracasts\Generators\GeneratorsServiceProvider::class,
+        AdamWathan\EloquentOAuthL5\EloquentOAuthServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
+        Jenssegers\Agent\AgentServiceProvider::class,
+        Cviebrock\EloquentSluggable\SluggableServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
 
     ],
 
@@ -201,6 +208,15 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        'OAuth'  => AdamWathan\EloquentOAuth\Facades\OAuth::class,
+        'Image'  => Intervention\Image\Facades\Image::class,
+        'Excel'  => Maatwebsite\Excel\Facades\Excel::class,
+        'Carbon' => Carbon\Carbon::class,
+        'Agent'  => Jenssegers\Agent\Facades\Agent::class,
+        'Form'   => Collective\Html\FormFacade::class,
+        'Html'   => Collective\Html\HtmlFacade::class,
+
 
     ],
 
